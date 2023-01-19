84438f93c578e105c1a29174a8211cc9e30c7470
keis19 <86911061+keis19@users.noreply.github.com>
2023-01-19 22:54:15 +0800

    Update EDA.py

--- pages/EDA.py
+++ pages/EDA.py
@@ -2,9 +2,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-    # Initialization
-    if 'key' not in st.session_state:
-      st.session_state['key'] = df_train
+# Initialization
+if 'key' not in st.session_state:
+  st.session_state['key'] = df_train
 
 df_ = df_train.groupby(by=[pd.Grouper(key="transaction_time", freq="1W"),
                            'is_fraud','category']).agg({"amount(usd)":'mean',"transaction_id":"count"}).reset_index()
