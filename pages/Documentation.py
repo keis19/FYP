546faa6eef5e62abeb0eb00c404b1b984502b33a
keis19 <86911061+keis19@users.noreply.github.com>
2023-01-19 22:07:29 +0800

    Update Documentation.py

--- pages/Documentation.py
+++ pages/Documentation.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('welcome To Something's Fishy emojis:fish:')
+st.title("'welcome To Something's Fishy emojis:fish:'")
 
 
 st.subheader("Information about the project")
