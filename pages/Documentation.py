f538340694676e14aea7e16c77d4ce70ad566768
keis19 <86911061+keis19@users.noreply.github.com>
2023-01-19 22:09:52 +0800

    Update Documentation.py

--- pages/Documentation.py
+++ pages/Documentation.py
@@ -10,7 +10,7 @@ st.write("In this current era, payments by credit card have become easier than e
 + "you detect the amount of fraud cases present within your dataset.")
 
 st.subheader("What can you find this application?")
-st.write (" Home page - Grey[Here you can upload your fie to get the number of fradulent cases]
+st.write (" Home page - Grey[Here you can upload your fie to get the number of fradulent cases]"
          + "EDA - Grey[Here you can find some interesting data visualization related to your dataset"
          + "Documentation - This page (The One You're In) is where you get details on how to use the application")
 
@@ -20,9 +20,9 @@ st.write( "1. Firstly upload your file by clicking Grey[Import file]. red[IMPORT
           + "3. You can then head to the Exploratory Data Analysis (EDA) section to view some fantastic insights obtained from the dataset.")
          
 st.subheader("Want to know the powerful Machine Learning Algorithm (ML) powering this application?")
-st.write("The algorithm that we are using to classify fraud cases is the Random Forest Algorithm (RFA). We had conducted a series of tests and the algorithm
-         + "with the best performance was the use RFA with datasets that have undergone Synthetics Minority Over-Sampling Technique (SMOTE) & Random Under Sampling).
-         + "Want to learn more about RFA click this [link] (https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html)
+st.write("The algorithm that we are using to classify fraud cases is the Random Forest Algorithm (RFA). We had conducted a series of tests and the algorithm"
+         + "with the best performance was the use RFA with datasets that have undergone Synthetics Minority Over-Sampling Technique (SMOTE) & Random Under Sampling)."
+         + "Want to learn more about RFA click this [link] (https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html)"
          
          
          
