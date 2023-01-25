96b434d3360b785f6f7309db1190da7818a5a4aa
keis19 <86911061+keis19@users.noreply.github.com>
2023-01-26 03:59:24 +0800

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -4,10 +4,9 @@ st.title("Welcome To Something's Fishy 🐠")
 
 
 st.subheader("Information about the project")
-st.write("In this current era, payments by credit card have become easier than ever. Although a multitude of improvements have been security has still"
-+ "major concern that has transenced with all the major upgrades. With the transactions by credit card and circulations of credit card growing rapidly" 
-+ "it calls upon for a better look into security. Our application aims to aid in this process by using top notch Machine Learning algorithms in helping" 
-+ "you detect the amount of fraud cases present within your dataset.")
+st.write("Credit card fraud detection is a pressing issue in the financial industry. With the increasing use of credit cards and online transactions, the opportunity for fraudulent activities also increases. Fraudulent activities in credit card transactions not only result in significant financial losses for individuals and businesses, but also undermine the trust in the financial system. The use of stolen or fake credit card information, phishing scams and skimming are just some examples of credit card fraud. It is crucial to detect and prevent these fraudulent activities in order to protect consumers and businesses from financial loss and to maintain trust in the financial system.
++ "The importance of tackling credit card fraud cannot be overstated. Financial institutions and businesses bear the direct financial loss from fraudulent activities, but consumers also suffer from the consequences. For example, their credit score may be negatively affected or they may have to go through the inconvenience of having their credit card cancelled and reissued. Furthermore, credit card fraud can also have a ripple effect on the economy by eroding trust in the financial system, which in turn can lead to reduced investment and spending."
++ "As the number and sophistication of fraudulent activities continue to increase, traditional methods of detection are becoming less effective. This is where machine learning models come in as a solution.")
 
 st.subheader("What can you find this application?")
 st.write ("Home page -Here you can upload your fie to get the number of fradulent cases")
